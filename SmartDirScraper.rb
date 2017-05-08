897287d591a980ab1d618339ed5c46a8f3fd6649
Noah Schechter <noahschechter7@gmail.com>
2017-05-08 15:48:00 -0400

    fixed the error on no results

--- SmartDirScraper.rb
+++ SmartDirScraper.rb
@@ -6,6 +6,7 @@ class SmartDirScraper
 	def initialize(url, search_term)
 		@base_url = url
 		@current_index = 1
+		@max_searches = 10000000
 		@search_term = search_term
 		@results = nil
 	end
@@ -15,31 +16,39 @@ class SmartDirScraper
 	end
 
 	def scrape_dir(url)
-		response = HTTParty.get(url)
-		case response.code
-		when 200
-			n_response = Nokogiri::HTML(response)
-			links = n_response.css('a')
-			links = links.select { |link| !has_extension?(link["href"]) }
-			@results = links.select { |link| link.text.upcase.include?(@search_term.upcase) }
-			if @results.empty? && links.length > 1
-				if url === @base_url
-					puts "Searching #{url + links[@current_index]['href']}"
-					scrape_dir(url + links[@current_index]["href"])
-				else
-					puts "Searching #{url + links[1]['href']}"
-					scrape_dir(url + links[1]["href"])
+		if @current_index < @max_searches
+			response = HTTParty.get(url)
+			case response.code
+			when 200
+				n_response = Nokogiri::HTML(response)
+				if (@max_searches === 10000000 && @base_url === url)
+					@max_searches = n_response.css('a').select { |link| !has_extension?(link["href"]) }.length
+					puts "Max Searches: #{@max_searches}"
 				end
-			elsif links.length === 1
-				@current_index += 1
-				scrape_dir(@base_url)
+				links = n_response.css('a')
+				links = links.select { |link| !has_extension?(link["href"]) }
+				@results = links.select { |link| link.text.upcase.include?(@search_term.upcase) }
+				if @results.empty? && links.length > 1
+					if url === @base_url
+						puts "Searching #{url + links[@current_index]['href']}"
+						scrape_dir(url + links[@current_index]["href"])
+					else
+						puts "Searching #{url + links[1]['href']}"
+						scrape_dir(url + links[1]["href"])
+					end
+				elsif links.length === 1
+					@current_index += 1
+					scrape_dir(@base_url)
+				else
+					@results.each { |result| puts "Found #{result.text} @ #{url + result['href']}" }
+				end
+			when 404
+				puts "ERROR - #{url} not found (Maybe try again later?)"
 			else
-				@results.each { |result| puts "Found #{result.text} @ #{url + result['href']}" }
+				puts "Uh oh..."
 			end
-		when 404
-			puts "ERROR - #{url} not found"
 		else
-			puts "Uh oh..."
+			puts "No results for #{@search_term} found."
 		end
 	end
 
