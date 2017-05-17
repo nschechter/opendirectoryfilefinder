58b3120590816516fa6788208f052d6bfe64e721
Noah Schechter <noahschechter7@gmail.com>
2017-05-17 17:30:33 -0400

    added a filter for h5ai directories. need to add support

--- DirectoryWrapper.rb
+++ DirectoryWrapper.rb
@@ -10,24 +10,31 @@ class DirectoryWrapper
 		# Retrive all the links in as a child of a th or td or li
 		links = dir.xpath('//th/a[not(child::*)]') + dir.xpath('//td/a[not(child::*)]') + dir.xpath('//li/a[not(child::*)]')
 
+		# If it's powered by h5ai, return false. Need to add support for it later.
+		meta_tags = dir.xpath('//head/meta')
+		if meta_tags && meta_tags.any? { |tag| tag['content'] && tag['content'].include?('h5ai') }
+			puts "Error - no support for h5ai"
+			return nil
+		end
+
 		# If we have detected a directory
 		if links.length > 0
 			# Filter the parent directory
 			links = links.select { |link| !DirectoryWrapper.is_parent?(link.text) }
 
 			# Filter out the links that route to more folders
-			dir_links = links.select { |link| DirectoryWrapper.has_directory_extension?(link["href"]) }
+			dir_links = links.select { |link| DirectoryWrapper.has_directory_extension?(link['href']) }
 
 			# Create new unscraped directories out of each of the child folders
 			dir_links.each do |link|
-				OpenDir.create!(url: url + link["href"], root_url: root_url, dir_type: link.text)
+				OpenDir.create!(url: url + link['href'], root_url: root_url, dir_type: link.text)
 			end
 
 			# Filter ou the links that route to files
-			file_links = links.select { |link| DirectoryWrapper.has_file_extension?(link["href"]) }
+			file_links = links.select { |link| DirectoryWrapper.has_file_extension?(link['href']) }
 
 			# Recreate the links to point only to their URL
-			file_links = file_links.map { |link| url + link["href"] }
+			file_links = file_links.map { |link| url + link['href'] }
 
 			return [dir_links, file_links]
 		else
